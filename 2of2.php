625ecddf4b99f60d2b0a1b8c113ccaca137ac4ae
mawadah alsayed <150618584+mawadah47@users.noreply.github.com>
2023-12-08 00:25:49 +0200

    Add files via upload

---
+++ 2of2.php
@@ -0,0 +1,16 @@
+<?php
+//printing the data type of the number 100
+
+// Method One
+echo (gettype(100));
+
+// Method Two
+//echo "<br";
+var_dump((integer)100);
+// Method Three => Optional
+$var=50;
+print (is_int($var));
+
+
+
+?>
