c898bd895dc188862697a749c25969454f137765
mawadah alsayed <150618584+mawadah47@users.noreply.github.com>
2023-12-08 01:06:19 +0200

    Add files via upload

---
+++ 5of2.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/* Needed Output
+
+Hello "'Elzero'"
+We Love $Programming$
+Languages Specially "PHP"
+
+*/
+
+echo nl2br( <<<'challeng'
+
+Hello "'Elzero'"
+We Love $Programming$
+Languages Specially "PHP"
+
+challeng );
+
+
+?>
